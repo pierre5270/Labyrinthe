0b553361731a69bc93197d7ad206d3ad3f775cd8
pierre5270 <pierre3signing@gmail.com>
2023-04-17 14:48:19 +0200

    phase 2

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include "block.hpp"
+#include "edge.hpp"
 
 int main (){
 
     block b = block(5) ;
     b.setNeighboor(2,1) ;
     cout << b << endl ;
+
+    edge e = edge(2,6) ;
+    cout << e << endl ;
     return EXIT_SUCCESS ;
 }
